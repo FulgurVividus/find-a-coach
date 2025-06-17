e831d398429e78877bdb592057b8faef46736e34
murodjon <murodjon.muzaffarov1@gmail.com>
2025-06-17 18:28:24 +0500

    feat: registering a coach (the form)

--- src/pages/coaches/CoachRegistration.vue
+++ src/pages/coaches/CoachRegistration.vue
@@ -1,3 +1,19 @@
 <template>
-  <section>REGISTER</section>
+  <section>
+    <base-card>
+      <h2>Register as a coach now!</h2>
+
+      <coach-form></coach-form>
+    </base-card>
+  </section>
 </template>
+
+<script>
+import CoachForm from "../../components/coaches/CoachForm.vue";
+
+export default {
+  components: {
+    "coach-form": CoachForm,
+  },
+};
+</script>
