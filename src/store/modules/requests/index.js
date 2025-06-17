ec31af2fbb48e679d3ca5d110da0d3bba9f5db7d
murodjon <murodjon.muzaffarov1@gmail.com>
2025-06-17 22:08:57 +0500

    feat: storing requests (messages) with vuex

---
+++ src/store/modules/requests/index.js
@@ -0,0 +1,16 @@
+import mutations from "./mutations";
+import actions from "./actions";
+
+export default {
+  namespaced: true,
+  //
+  state() {
+    return {
+      requests: [],
+    };
+  },
+  //
+  mutations: mutations,
+  //
+  actions: actions,
+};
