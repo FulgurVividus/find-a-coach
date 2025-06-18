e4074b9e199bff83ca66a8fb4806b5b1d80d3b80
murodjon <murodjon.muzaffarov1@gmail.com>
2025-06-18 21:54:55 +0500

    feat: fetching all coach data

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -6,7 +6,7 @@
   <section>
     <base-card>
       <div class="controls">
-        <base-button mode="outline">Refresh</base-button>
+        <base-button mode="outline" @click="loadCoaches">Refresh</base-button>
         <base-button to="/register" link v-if="!isCoach"
           >Register as Coach</base-button
         >
@@ -78,6 +78,13 @@ export default {
     setFilters(updatedFilters) {
       this.activeFilters = updatedFilters;
     },
+    loadCoaches() {
+      this.$store.dispatch("coaches/loadCoaches");
+    },
+  },
+  //
+  created() {
+    this.loadCoaches();
   },
 };
 </script>
