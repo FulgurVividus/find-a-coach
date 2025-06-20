d4d4f1e27ce2254d954ec25b97b69cc2036931b7
murodjon <murodjon.muzaffarov1@gmail.com>
2025-06-20 21:27:56 +0500

    feat: added login action & flow

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,5 +1,29 @@
 export default {
-  login() {},
+  async login(context, payload) {
+    const response = await fetch(
+      `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDTh53gYyxeov8-X63a-jKuktGra1LyPYc`,
+      {
+        method: "POST",
+        body: JSON.stringify({
+          email: payload.email,
+          password: payload.password,
+          returnSecureToken: true,
+        }),
+      }
+    );
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      const error = new Error(responseData.message || "Failed to signup!");
+      throw error;
+    }
+
+    context.commit("setUser", {
+      token: responseData.idToken,
+      userId: responseData.localId,
+      tokeExpiration: responseData.expiresIn,
+    });
+  },
   async signup(context, payload) {
     const response = await fetch(
       `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDTh53gYyxeov8-X63a-jKuktGra1LyPYc`,
